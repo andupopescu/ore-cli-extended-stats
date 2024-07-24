368670bc6e7156d2714085c60d61f23d1b5f0782
Andrei Popescu <andrei.popescu@thenavpeople.co.uk>
2024-07-24 21:43:20 +0100

    changed walletBalance.sh to format correctly on MAC

--- walletBalance.sh
+++ walletBalance.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-#
-if [ $# -ne 1 ]; then
-	echo "USAGE: $0 [wallet/miner integer]"
-	exit 1
+
+if [[ $# -eq 0 || $# -gt 2 ]]; then
+    echo "USAGE: $0 [wallet/miner integer] [log version 1-6]"
+    exit 1
 fi
+
 source ./ore_env.sh $1
+VERSION=${2:-1}
 
-SHORT_KEY=$(basename ${KEY})
+STATS_LOGFILE_BASE="./logs/${MINER_NAME// /_}"
 
-echo -n "`date +'%Y%m%d%H%M%S'` ${SHORT_KEY} "
-balance=$(${ORE_BIN} --rpc ${RPC_URL} --keypair ${KEY} balance)
-retval1=$?
-if [ ${retval1} -ne 0 ]; then
-	echo "ERROR: Failed to retrieve the balance"
-	exit ${retval1}
-fi
-balanceVal=$(echo ${balance} | awk '{printf("%.11f", $2)}')
-stakedVal=$(echo ${balance} | awk '{printf("%.11f", $5)}')
-./coingeckoDownloadPrice.sh Ore quiet
-oreDollars=$(cat ./currentPriceOfOre.txt)
-balanceDollars=$(echo "${balanceVal} * ${oreDollars}" | bc )
-balanceDollars=$(printf "%.2f" ${balanceDollars})
-stakedDollars=$(echo "${stakedVal} * ${oreDollars}" | bc )
-stakedDollars=$(printf "%.2f" ${stakedDollars})
-echo "Wallet $1 ORE balance: ${balanceVal} ORE (\$${balanceDollars})	Staked: ${stakedVal} ORE (\$${stakedDollars})"
+while true; do
+    clear
+    echo "Displaying log $(ls ${STATS_LOGFILE_BASE}--${VERSION}--*.log)"
+    cat ${STATS_LOGFILE_BASE}--${VERSION}--*.log
+    sleep 5
+done
